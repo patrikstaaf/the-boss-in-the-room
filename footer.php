ea1855200eb1b51de69e88cd45aecc3874fd58c6
patrikstaaf <staaf.patrik@gmail.com>
2021-10-24 13:12:37 +0200

    Added nav and styling to footer.

--- footer.php
+++ footer.php
@@ -1,5 +1,24 @@
 <footer>
-    <small>&copy; <?= date("Y"); ?></small>
+    <!-- <div class="py-12 px-4 flex flex-row mx-auto container justify-center"> -->
+
+    <!-- <div class="mx-auto container flex flex-col items-center justify-center"> -->
+
+    <div class="text-black flex flex-col items-center justify-center py-6">
+        <img class="w-20 h-60" src="/springsteen.svg" alt="Bruce Springsteen with a guitar on his back">
+        <h2 class="focus:outline-none text-2xl font-black">Simply. The Boss.</h2>
+        <div class="my-6 text-base text-color f-f-l">
+            <ul class="flex flex-row space-x-6">
+                <li class="cursor-pointer pt-4"><a href="/albums.php" class="focus:outline-none focus:underline hover:text-gray-500">Albums </a></li>
+                <li class="cursor-pointer pt-4"><a href="/tracks.php" class="focus:outline-none focus:underline hover:text-gray-500">Tracks </a></li>
+                <li class="cursor-pointer pt-4"><a href="/concerts.php" class="focus:outline-none focus:underline hover:text-gray-500">Concerts </a></li>
+                <li class="cursor-pointer pt-4"><a href="/quiz.php" class="focus:outline-none focus:underline hover:text-gray-500">Quiz </a></li>
+            </ul>
+        </div>
+        <div class="text-sm text-color f-f-l">
+            <p><a href="https://github.com/patrikstaaf">Patrik Staaf </a>&copy; <?= date("Y"); ?></p>
+        </div>
+    </div>
+    </div>
 </footer>
 </body>
 

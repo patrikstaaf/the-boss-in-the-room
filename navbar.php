9ea15465cc9594fc39cf73eaa78a66680f97a112
patrikstaaf <staaf.patrik@gmail.com>
2021-10-24 10:04:04 +0200

    Added structure, responsiveness and basic styling to the nav bar.

--- navbar.php
+++ navbar.php
@@ -1,20 +1,42 @@
-<nav class="flex justify-between">
-    <!-- <div class="flex gap-1 items-baseline"> -->
-    <!-- <div class="w-1 h-5 rounded-md bg-pink-400"></div> -->
-    <p class="text-[24px] font-bold">Bruce Springsteen</p>
-    <!-- </div> -->
+<nav class="bg-gray-100 fixed top-0 z-10 inset-x-0">
+    <div class="max-w-6xl mx-auto px-4">
+        <div class="flex justify-between">
 
-    <div class="flex flex-col gap-1 justify-center md:hidden">
-        <div class="h-1 w-8 rounded-md bg-white"></div>
-        <div class="h-1 w-8 rounded-md bg-white"></div>
-        <div class="h-1 w-8 rounded-md bg-white"></div>
+            <div class="flex space-x-4">
+                <!-- logo -->
+                <div>
+                    <a href="/index.php" class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
+                        <img class="h-8 w-8 mr-1" src="/springsteen.svg" alt="">
+                        <span class="text-2xl font-bold">Bruce Springsteen</span>
+                    </a>
+                </div>
+            </div>
+
+            <!-- nav items -->
+            <div class="hidden md:flex items-center space-x-1">
+                <a href="/albums.php" class="py-5 px-3 text-gray-700 hover:text-gray-900">Albums</a>
+                <a href="/tracks.php" class="py-5 px-3 text-gray-700 hover:text-gray-900">Tracks</a>
+                <a href="/concerts.php" class="py-5 px-3 text-gray-700 hover:text-gray-900">Concerts</a>
+                <a href="/quiz.php" class="py-2 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300">Quiz</a>
+            </div>
+
+            <!-- mobile button goes here -->
+            <div class="md:hidden flex items-center">
+                <button class="mobile-menu-button">
+                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+
+        </div>
     </div>
 
-    <div class="hidden md:flex gap-10 items-center">
-        <a href="/albums.php">Albums</a>
-        <a href="/tracks.php">Tracks</a>
-        <a href="/concerts.php">Concerts</a>
-        <a href="/quiz.php">Quiz</a>
-
+    <!-- mobile menu -->
+    <div class="mobile-menu hidden md:hidden bg-white">
+        <a href="/albums.php" class="block py-2 px-4 text-sm hover:bg-gray-200">Albums</a>
+        <a href="/tracks.php" class="block py-2 px-4 text-sm hover:bg-gray-200">Tracks</a>
+        <a href="/concerts.php" class="block py-2 px-4 text-sm hover:bg-gray-200">Concerts</a>
+        <a href="/quiz.php" class="block py-2 px-4 text-sm hover:bg-gray-200">Quiz</a>
     </div>
 </nav>
